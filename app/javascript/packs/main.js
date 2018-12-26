fb06656fb5932e4603165cd9afad9d0db2aa6940
Lucas Videla <videla.lucas@gmail.com>
2018-12-26 15:53:07 -0300

    Analytics!

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
+
 import VueRouter from 'vue-router';
 Vue.use(VueRouter);
+
 import vueHeadful from 'vue-headful';
 Vue.component('vue-headful', vueHeadful);
+
 import Vue2Filters from 'vue2-filters';
 Vue.use(Vue2Filters);
+
+import VueAnalytics from 'vue-analytics';
+Vue.use(VueAnalytics, {
+  id: 'UA-131375892-1'
+})
 
 import App from '../components/App.vue'
 import router from '../router'
